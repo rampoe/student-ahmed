48f8c40d6e6ee7e02a57aa3a1150d59b8a2e6d2e
rampoe <mail.rampoe@gmail.com>
2023-10-30 19:55:49 +0500

    hw

--- 10.23.2023/hw.py
+++ 10.23.2023/hw.py
@@ -1,3 +1,5 @@
+import random
+
 # есть следующий словарь:
 contacts = {
     "Rahman": "+993654654654",
@@ -10,3 +12,10 @@ contacts = {
 # при каждом нажатии enter должен показываться один рандомный контакт
 # показанный контакт должен удаляться из словаря и никогда не должен показываться снова
 # программа должна прекращать свое исполнение как только все контакнты были показаны и удалены
+
+while contacts:
+    keys_list = list(contacts)
+    random_key = keys_list[random.randrange(len(keys_list))]
+    print(f"{random_key}: {contacts[random_key]}")
+    input()
+    del contacts[random_key]
