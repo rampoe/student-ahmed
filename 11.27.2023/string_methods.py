da339c7796c1660a4a1ce09609ec2c65a85c4cd6
rampoe <mail.rampoe@gmail.com>
2023-11-30 20:20:27 +0500

    practice

---
+++ 11.27.2023/string_methods.py
@@ -0,0 +1,39 @@
+# sample_string = "Hello, World!"
+
+# print(len(sample_string))
+# print(sample_string.find("o"))
+# print(sample_string.capitalize())
+# print(sample_string.upper())
+# print(sample_string.lower())
+# print(sample_string.count("o"))
+
+# print(sample_string.islower())
+# print(sample_string.isupper())
+# print(sample_string[0].islower())
+# print(sample_string[0].isupper())
+
+# s = "⅓"
+# print(s.isdigit())
+# print(s.isdecimal())
+
+# s = "rahm123an"
+# print(s.isalpha())
+# print(s.isalnum())
+
+# s = "                   12341234                   "
+# print(s.isdigit())
+# print(s.strip())
+# print(s.lstrip())
+# print(s.rstrip())
+
+# s = "Rahman"
+# print(s.lower())
+
+# arr = ["Rahman", "loves", "Python", "!"]
+# print("".join(arr))
+
+# s = "Rahman loves Python!"
+# arr = s.split("o")
+# print(arr)
+
+# print("JavaScript is the best programming language!".replace("JavaScript", "Python"))
